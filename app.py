17d4b6f0ed86249cb01c0c447928dbf660fc037a
Samir Alibabic <samir.alibabic@gmail.com>
2024-09-15 23:10:17 +0200

    Use PORT environment variable in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from crawler import crawl_url, crawl_domain
+import os
 
 app = Flask(__name__)
 
@@ -25,4 +26,5 @@ def crawl():
     return jsonify(list(external_links))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
